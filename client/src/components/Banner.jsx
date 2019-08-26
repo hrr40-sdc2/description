56162ad0b1121b51936b4f4639729ec07b29a3d2
Keitel Jovin <keiteldog@gmail.com>
2019-08-26 15:20:35 -0700

    Use styled components in Search, Menu and Banner

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
+
+
+const BannerContainer = styled.div`
+  position: relative;
+  /*TODO: remove border when ready*/
+  border-top: 1px solid #ccc;
+  border-bottom: 1px solid #ccc;
+`;
+
+const PhotosContainer = styled.div`
+  min-height: 290px;
+`;
+
+const CommandsContainer = styled.div`
+  position:absolute;
+  top: 10px;
+  right: 10px;
+  height: 90%;
+  width: 200px;
+  /*TODO: remove border when ready*/
+  border: 1px solid #ccc;
+`;
+
 
 const Banner = (props) => {
 
   return (
-    <div id="overview-house-banner" >
-      <div className="floating-commands" >
+    <BannerContainer id="overview-house-banner" >
+      <CommandsContainer className="floating-commands" >
         Floating Commands
-      </div>
-      <div className="photos-container" >
+      </CommandsContainer>
+      <PhotosContainer className="photos-container" >
         <center>Photos Container</center>
-      </div>
-    </div>
+      </PhotosContainer>
+    </BannerContainer>
   );
 };
 
--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,16 +1,44 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Navigation = styled.nav`
+  margin: 0px;
+  float: right;
+`;
+
+const NavList = styled.ul`
+  margin: 0;
+  padding: 0;
+  display: inline-block;
+  list-style-type: none;
+  height:80px;
+`;
+
+const NavItem = styled.li`
+  margin: 2px 10px;
+  padding: 0 10px;
+  display: inline-block;
+  line-height: 80px;
+  height: 74px;
+  font-weight: 600;
+  cursor: pointer;
+
+  &:hover {
+    border-bottom: 2px solid #484848;
+  }
+`;
 
 const Menu = (props) => {
 
   return (
-    <nav id="overview-house-menu" >
-      <ul>
-        <li>Become a Host</li>
-        <li>Help</li>
-        <li>Signup</li>
-        <li>Login</li>
-      </ul>
-    </nav>
+    <Navigation id="overview-house-menu" >
+      <NavList>
+        <NavItem>Become a Host</NavItem>
+        <NavItem>Help</NavItem>
+        <NavItem>Signup</NavItem>
+        <NavItem>Login</NavItem>
+      </NavList>
+    </Navigation>
   );
 };
 
