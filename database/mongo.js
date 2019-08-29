e37220602359a33f51270139b1a2cadf38a8c902
Keitel Jovin <keiteldog@gmail.com>
2019-08-29 00:59:44 -0700

    Debug MongoDB in Circle CI

--- database/mongo.js
+++ database/mongo.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
-const mongoUri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/house';
+const mongoUri = process.env.MONGO_URI || 'mongodb://localhost/house';
 module.exports = mongoose.connect(mongoUri);
