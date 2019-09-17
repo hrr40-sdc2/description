8314606f112d434f7f9994fbd4afdb791ac11894
Mr Myers <myersniiansah@gmail.com>
2019-09-16 23:47:00 -0400

    initial couch file

---
+++ couchdb/House.js
@@ -0,0 +1,15 @@
+CREATE TABLE House
+(
+  id varchar not null,
+  title varchar not null,
+  location varchar not null,
+  private_room,
+  super_host_name varchar not null,
+  super_host_photo varchar not null,
+  rating  varchar not null,
+  desc varchar not null,
+  space_desc varchar not null,
+  guest_desc varchar not null,
+  other_desc not null,
+  amenities varchar not null
+)
