77911b2af443611e3ffef4e2d6e9a9db129272e0
Mr Myers <myersniiansah@gmail.com>
2019-09-19 18:56:53 -0400

    removed values that added commas

--- dataGenerator/dataGenerator.js
+++ dataGenerator/dataGenerator.js
@@ -12,10 +12,11 @@ const amenData = fs.createWriteStream('./amenities.csv', {encoding: 'utf8'});
 
 var createHouse = () => {
   (async() => {
-    houseData.write('id,title,location,super_host_name,super_host_photo, rating,desc,space_desc,guest_desc,other_desc\n');
+    houseData.write('id,title,is_entire_place,location,super_host_name,super_host_photo, rating,desc,space_desc,guest_desc,other_desc\n');
     for (var i = 1; i <= records; i++) {
         const id = i;
         const title = faker.lorem.words();
+        const is_entire_place = faker.random.boolean();
         const location = faker.address.city();
         const super_host_name = faker.name.firstName();
         const super_host_photo = faker.image.imageUrl();
@@ -24,7 +25,7 @@ var createHouse = () => {
         const space_desc = faker.lorem.words();
         const guest_desc = faker.lorem.words();
         const other_desc = faker.lorem.words();
-        const House = `${id},${title},${location},${super_host_name},${super_host_photo},${rating},${desc},${space_desc},${guest_desc},${other_desc}\n`;
+        const House = `${id},${title},${is_entire_place},${location},${super_host_name},${super_host_photo},${rating},${desc},${space_desc},${guest_desc},${other_desc}\n`;
         if (!houseData.write(House, {flag: 'r+'})) {
           await new Promise(resolve => houseData.once('drain', resolve));
         }
@@ -34,13 +35,14 @@ var createHouse = () => {
 
 var createRooms = () => {
   (async() => {
-    roomData.write('house_id,guests,size,bath\n');
+    roomData.write('id,house_id,guests,size,bath\n');
     for (var i = 1; i <= records * 2; i++) {
+      const id = i;
       const house_id = i;
       const guests = faker.random.number({min: 1, max: 10});
       const size = faker.lorem.word();
       const bath = faker.random.number({min: 1, max: 3});
-      const Room = `${house_id},${guests},${size},${bath}\n`;
+      const Room = `${id},${house_id},${guests},${size},${bath}\n`;
 
       if (!roomData.write(Room, {flag: 'r+'})) {
         await new Promise(resolve => roomData.once('drain', resolve));
@@ -58,7 +60,7 @@ var createPhotos = () => {
       const id = i;
       const house_id = faker.random.number({min: 1, max: 10000000});
       const file_path = `https://hrr40-sdc-images.s3.us-east-2.amazonaws.com/sdc-house-images/houseimage${faker.random.number({min: 1, max: 1000})}.jpg`;
-      const desc = faker.random.word();
+      const desc = faker.lorem.word();
       const Photo = `${id},${house_id},${file_path}, ${desc}\n`;
       if (!photoData.write(Photo, {flag: 'r+'})) {
         await new Promise(resolve => photoData.once('drain', resolve));
@@ -73,8 +75,8 @@ var createAmenities = () => {
     for (var i = 1; i <= records * 2; i++) {
       const id = i;
       const house_id = faker.random.number({min: 1, max: 10000000});
-      const category = faker.random.word();
-      const item = faker.random.word();
+      const category = faker.lorem.word();
+      const item = faker.lorem.word();
       const desc = faker.lorem.words();
       const Amenities = `${id},${house_id},${category},${item},${desc}\n`;
       if (!amenData.write(Amenities, {flag: 'r+'})) {
