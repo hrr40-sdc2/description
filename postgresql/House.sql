4ec29b335c055cfc176607762c1b35d3a827edbd
Mr Myers <myersniiansah@gmail.com>
2019-09-19 18:57:31 -0400

    changed schema to incorporate integers and room ids

--- postgresql/House.sql
+++ postgresql/House.sql
@@ -4,10 +4,11 @@ CREATE TABLE homes
 (
   id                        SERIAL PRIMARY KEY,
   title                     VARCHAR(256),
+  is_entire_place           VARCHAR(10),
   location                  VARCHAR(256),
   super_host_name           VARCHAR(20),
   super_host_photo          VARCHAR(128),
-  rating                    VARCHAR(5),
+  rating                    INTEGER,
   description               VARCHAR(256),
   space_desc                VARCHAR(256),
   guest_desc                VARCHAR(256),
@@ -17,7 +18,7 @@ CREATE TABLE homes
 CREATE TABLE photos
 (
     id              SERIAL PRIMARY KEY,
-    home_id         VARCHAR(256),
+    home_id         INTEGER,
     file_path       VARCHAR(256),
     description     VARCHAR(256)
 );
@@ -25,7 +26,8 @@ CREATE TABLE photos
 CREATE TABLE bedrooms
 (
     id              SERIAL PRIMARY KEY,
-    home_id         VARCHAR(256),
+    home_id         INTEGER,
+    guests          INTEGER,
     size            VARCHAR (256),
     bath            VARCHAR(10)
 );
@@ -33,7 +35,7 @@ CREATE TABLE bedrooms
 CREATE TABLE amenities
 (
     id              SERIAL  PRIMARY KEY,
-    home_id         VARCHAR(256),
+    home_id         INTEGER,
     category        VARCHAR(256),
     item            VARCHAR(256),
     description     VARCHAR(256)
