39c5826a384d3308d25e7f4ebc5a6ae4c9945f0f
Mr Myers <myersniiansah@gmail.com>
2019-09-24 11:20:51 -0400

    changed routes for requests

--- stressTest/getTest.js
+++ stressTest/getTest.js
@@ -10,7 +10,7 @@ export let options = {
 };
 
 export default function() {
-  let res = http.get("http://localhost:3010");
+  let res = http.get("http://localhost:3010/api/houses/9123456");
   check(res, {
     "status was 200": (r) => r.status == 200,
     "transaction time OK": (r) => r.timings.duration < 200
